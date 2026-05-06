a90c4a68d26372b387bae066921ab813ea5469df
robertbenitezek <robertbenitezek@gmail.com>
2026-05-05 23:32:29 -0700

    pymodbus communication slightly working

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from pymodbus.client import ModbusSerialClient
+from pymodbus.client.sync import ModbusSerialClient
 import serial
 
 client = ModbusSerialClient(
+    method='rtu',
     port='/dev/rs485',  # COM port for Windows or /dev/ttyUSBX for Linux
     baudrate=115200,        # Communication speed
     parity='N',           # N=None, E=Even, O=Odd
-    stopbits=1,           # Number of stop bits
+    stopbits=2,           # Number of stop bits
     bytesize=8,           # Number of data bits
     timeout=1             # Seconds to wait for a response
 )
 
-#parameters from datasheet are tbd
+try:
+	print("Connecting to servo drive...")
+	client.connect()
+	print("connected to servo drive")
+	
+	# Switch to speed control mode
+	client.write_register(address=0, value=1, unit=1)
+
+	#set speed source to rs485
+	client.write_register(address=1280, value=5, unit=1)
+
+	r1=client.read_holding_registers(address=0, count=1, unit=1)
+	r2=client.read_holding_registers(address=1280, count=1, unit=1)
+
+	print(f"P00.00: {r1.registers[0]}")
+	print(f"P05.00: {r2.registers[0]}")
+
+finally:
+	print("disconnecting from servo drive...")
+	client.close()
+	print("disconnected from servo driver")
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
-This is the third commit for the hockey robot.
+This is the fourth commit for the hockey robot.
 
-Hopefully can keep it up and work out a good development workflow on the jetson nano.
+My goal now is to have one commit a day to show my progress.
 
-Next steps are to read and write values from the modbus table to this servo motor. Once this is complete figure out how to create a homing sequence such that the device does not get lost in the sauce
+Initially my next steps were to read and write values from the modbus table to this servo motor. Once this is complete figure out how to create a homing sequence such that the device does not get lost in the sauce
+
+so i completed (somewhat) the first task here, i was able to finally connect to the servo motor via pymodbus but for some reason im running into some parameter reading issues. so will need to look into that tomororow. by tomorrow my goal is to be able to control the servo motor spinning while reading the position of the servo in real time.
+
